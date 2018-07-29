93846376d004951a4f3eb36697fc2b47aec7a45a
Sahand <sahandkarimxani@gmail.com>
2018-07-29 17:11:56 +0430

    add two new models and add poll app to setting.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField('date of pub')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
