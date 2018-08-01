d3ae36e367e521290e157d341ef00b56f4261682
Sahand <sahandkarimxani@gmail.com>
2018-08-01 21:59:38 +0430

    use render shortcut to create HttpResponse object

--- polls/views.py
+++ polls/views.py
@@ -6,11 +6,11 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list' : latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html',context)
 
 def detail(request, question_id):
     return HttpResponse("you are looking at question {}".format(question_id))
