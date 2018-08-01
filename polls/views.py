9badb30cd37d50e60617d1923492f90049281e36
Sahand <sahandkarimxani@gmail.com>
2018-08-02 00:17:22 +0430

    end of third tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Question
 
@@ -13,7 +13,16 @@ def index(request):
     return render(request, 'polls/index.html',context)
 
 def detail(request, question_id):
-    return HttpResponse("you are looking at question {}".format(question_id))
+    # try:
+        # question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:   
+        # raise Http404("Question not found")
+    question = get_object_or_404(Question, pk=question_id)
+    
+    context = {
+        'question' : question, 
+    }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "this page shows reults of question {}"
